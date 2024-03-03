2f50ccd74a49be1f0bd74f70b153674f5f798376
Williamsatse <52837884+Williamsatse@users.noreply.github.com>
2024-03-03 00:24:17 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function onButton()
+{
+    console.log('Buton cliqué')
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "on", true)
+    xhttp.send();
+}
+function offButton()
+{
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "off", true)
+    xhttp.send();
+}
+
+function onButton () {
+    
+
+}
